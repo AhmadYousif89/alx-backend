28bb00199eac116796a750bb51c81e5a60af809f
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-27 15:26:42 +0300

    Refactor Flask app to include Babel i18n configuration

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,28 +3,27 @@
 Basic Flask app with a single route
 """
 
-from flask import Flask
 from flask_babel import Babel
-from flask import render_template
+from flask import Flask, render_template
 
-# app = Flask(__name__)
-# app.url_map.strict_slashes = False
-# bable = Babel(app)
-
-
-# @app.route('/', methods=['GET'])
-# def index() -> str:
-#     """Main page for the Flask app"""
-#     return render_template('1-index.html')
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+bable = Babel(app)
 
 
-# class Config:
-#     """Babel i18n configuration"""
-
-#     LANGUAGES = ["en", "fr"]
-#     BABEL_DEFAULT_LOCALE = "en"
-#     BABEL_DEFAULT_TIMEZONE = "UTC"
+@app.route('/', methods=['GET'])
+def index() -> str:
+    """Main page for the Flask app"""
+    return render_template('1-index.html')
 
 
-# if __name__ == "__main__":
-#     app.run()
+class Config:
+    """Babel i18n configuration"""
+
+    LANGUAGES: list[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+if __name__ == "__main__":
+    app.run()
