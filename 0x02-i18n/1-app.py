0135592b239c1518eec8d1faf4414522dd786b3a
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-27 15:23:30 +0300

    Refactor imports and fix app.run() placement

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,24 +7,24 @@ from flask import Flask
 from flask_babel import Babel
 from flask import render_template
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-bable = Babel(app)
+# app = Flask(__name__)
+# app.url_map.strict_slashes = False
+# bable = Babel(app)
 
 
-@app.route('/', methods=['GET'])
-def index() -> str:
-    """Main page for the Flask app"""
-    return render_template('1-index.html')
+# @app.route('/', methods=['GET'])
+# def index() -> str:
+#     """Main page for the Flask app"""
+#     return render_template('1-index.html')
 
 
-class Config:
-    """Babel i18n configuration"""
+# class Config:
+#     """Babel i18n configuration"""
 
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+#     LANGUAGES = ["en", "fr"]
+#     BABEL_DEFAULT_LOCALE = "en"
+#     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
