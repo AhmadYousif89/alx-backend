03439410c032456ca3a81c4e7fadc841bd9407bb
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-27 15:33:10 +0300

    Refactor i18n configuration and update index template

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -6,31 +6,32 @@ Basic Flask app with a single route
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
+
+class Config:
+    """Babel i18n configuration"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.config.from_object(Config)
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale"""
+    best_match = request.accept_languages.best_match(app.config['LANGUAGES'])
+    return best_match if best_match else app.config['BABEL_DEFAULT_LOCALE']
 
 
 @app.route('/', methods=['GET'])
 def index() -> str:
     """Main page for the Flask app"""
-    return render_template('1-index.html')
-
-
-class Config:
-    """Babel i18n configuration"""
-
-    LANGUAGES: list[str] = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-    @babel.localeselector
-    def get_locale() -> str:
-        """Get locale"""
-        best_match = request.accept_languages.best_match(
-            app.config['LANGUAGES']
-        )
-        return best_match if best_match else app.config['BABEL_DEFAULT_LOCALE']
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
